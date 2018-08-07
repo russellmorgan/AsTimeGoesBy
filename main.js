93207b9eee704d8ada69f851b7e0103e9f4cb6d5
Russell Morgan <russell.morgan@contractos.lesschwab.com>
2018-08-07 14:24:35 -0400

    Functionality complete!

--- main.js
+++ main.js
@@ -1,29 +1,57 @@
 class TimeLeftClass {
     constructor() {
       this.userLife = 1081 //90 years, let user pick later
-      //put userInfo here in the constructor once everything else is working...
+      this.userInfo = localStorage.getItem('userInfo');
     }
     init() {
-      let userInfo = localStorage.getItem('userInfo');
-      if(userInfo) {
-        this.showUserData(userInfo);
+      this.displayData();
+      this.deleteUserData();
+      this.collectUserData();
+    }
+    displayData() {
+      if(this.userInfo) {
+        //Make sure the data is in JSON format
+        this.userInfo = JSON.parse(localStorage.getItem('userInfo'));
+        document.getElementsByClassName("userinfo__form")[0].style.display = 'none';
+        document.getElementsByClassName("userinfo__details")[0].style.display = 'flex';
+        this.showUserData();
       } else {
-        this.collectUserData()
+        document.getElementsByClassName("userinfo__form")[0].style.display = 'flex';
+        document.getElementsByClassName("userinfo__details")[0].style.display = 'none';
       }
+      
+    }
+    deleteUserData() {
+      document.getElementById("reset").addEventListener("click", (event) => {
+        localStorage.removeItem('userInfo');
+        this.userInfo = undefined;
+        const grid = document.getElementById('timeleft');
+        while (grid.firstChild) {
+          grid.removeChild(grid.firstChild);
+        }
+        event.preventDefault();
+        this.displayData();
+      });
     }
     collectUserData() {
-      let userForm = document.getElementsByClassName("userinfo__form");
-      document.getElementById("userSubmit").addEventListener("click", function() {
+      document.getElementById("submit").addEventListener("click", (event) => {
         //Save user data in localstorage
         let name = document.getElementById("userName").value;
         let month = document.getElementById("userMonth").value;
         let year = document.getElementById("userYear").value;
+        //TODO: Validate user entered something...
         const userInfo = { userName: name, userMonth: month, userYear: year };
-        localStorage.setItem("userInfo", JSON.stringify(userInfo));
-        this.buildGrid();
+        localStorage.setItem('userInfo', JSON.stringify(userInfo));
+        this.userInfo = localStorage.getItem('userInfo');
+        this.displayData();
+        event.preventDefault();
       });
     }
-    showUserData(userInfo) {
+    showUserData() {
+      //Display current user data
+      //TODO: Show date string instead of number with momentjs
+      document.getElementsByClassName("userinfo__name")[0].textContent = `Hello ${this.userInfo.userName}`;
+      document.getElementsByClassName("userinfo__birth")[0].textContent = `Birthdate: ${this.userInfo.userMonth}, ${this.userInfo.userYear}`;
       this.buildGrid()
     }
     buildGrid() {
@@ -37,9 +65,7 @@ class TimeLeftClass {
       this.showStatus();
     }
     showStatus() {
-      let userInfo = JSON.parse(localStorage.getItem('userInfo'));
-      //const userMoment = `${userInfo.userYear} ${userInfo.userMonth}`;
-      let userBirth = moment(`${userInfo.userYear} ${userInfo.userMonth}`, 'YYYY MM');
+      let userBirth = moment(`${this.userInfo.userYear} ${this.userInfo.userMonth}`, 'YYYY MM');
       let monthsUsed = userBirth.diff(moment(), 'months')*-1;
       let monthsGrid = document.getElementsByClassName('month');
       for (let i=0; i < monthsUsed; i++) {
